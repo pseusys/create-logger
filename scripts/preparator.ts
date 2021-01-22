9d263eaccb5d186dec43cb705c2c58ef02602f29
milesartemius <shveitsar215@gmail.com>
2021-01-22 19:02:09 +0300

    preparator prepared

--- scripts/preparator.ts
+++ scripts/preparator.ts
@@ -1,35 +1,31 @@
-function insert(str: string, substr: string, pos: number): string {
-    return str.slice(0, pos) + substr + str.slice(pos);
-}
-
-
-
-function getHead(element: HTMLElement): string {
-    const html = element.outerHTML;
-    return html.slice(0, html.indexOf('>') + 1);
-}
-
-function getTail(element: HTMLElement): string {
-    const html = element.outerHTML;
-    return html.slice(html.lastIndexOf('<'));
-}
-
-function splitAt(element: HTMLElement, pos: number) {
-    const head = getHead(element);
-    const tail = getTail(element);
-    element.outerHTML = insert(element.outerHTML, tail + head, head.length + pos);
-}
-
-
-
-function childNum(node: HTMLElement): number {
-    let pred = node.previousSibling;
-    let sum = 0;
-    while (pred != null) {
-        sum++;
-        pred = pred.previousSibling;
+function splitAt(element: HTMLElement, parent: HTMLElement, pos: number, postInsertRange?: Range) {
+    const clone = element.cloneNode(true) as HTMLElement;
+    if (!postInsertRange) {
+        clone.innerText = element.innerText.slice(0, pos);
+        parent.insertBefore(clone, element);
+        element.innerText = element.innerText.slice(pos);
+    } else {
+        clone.innerText = element.innerText.slice(pos);
+        parent.insertBefore(clone, element.nextSibling);
+        element.innerText = element.innerText.slice(0, pos);
+        postInsertRange.setEnd(clone, 0);
     }
-    return sum;
+}
+
+function isCuttingStart(offset: number, start: HTMLElement) {
+    return (offset == 0) || (offset == start.innerText.length);
+}
+
+function isCuttingEnd(offset: number, end: HTMLElement) {
+    return (offset == 0) || (offset == end.innerText.length);
+}
+
+function isThisStart(node: Element, start: HTMLElement, offset: number) {
+    return (node.isEqualNode(start)) && (offset != start.innerText.length);
+}
+
+function isThisEnd(node: Element, start: HTMLElement, offset: number) {
+    return (node.isEqualNode(start)) && (offset != 0);
 }
 
 
@@ -68,71 +64,46 @@ function changeClass(elem: Element, cls: string, val: string): void {
 function change(format: Formatting): void {
     const sel = document.getSelection();
     const range = sel.getRangeAt(0);
-    let firstElem = getTargetSpan(range.startContainer);
-    let lastElem = getTargetSpan(range.endContainer);
-
+    const firstElem = getTargetSpan(range.startContainer);
+    const lastElem = getTargetSpan(range.endContainer);
     let parent: HTMLElement;
-    let startPos = childNum(firstElem);
-    let endPos = childNum(lastElem);
 
     if (firstElem.isSameNode(lastElem)) {
         parent = firstElem.parentElement as HTMLElement;
         const finalOffset = range.endOffset - range.startOffset;
-
-        if (range.startOffset != 0) {
-            splitAt(firstElem, range.startOffset);
-            lastElem = parent.children[endPos] as HTMLElement;
-            startPos++;
-            endPos++;
-        }
-        splitAt(lastElem, finalOffset);
-        endPos++;
+        if (!isCuttingStart(range.startOffset, firstElem)) splitAt(firstElem, parent, range.startOffset);
+        if (!isCuttingEnd(finalOffset, lastElem)) splitAt(lastElem, parent, finalOffset, range);
     } else {
         parent = range.commonAncestorContainer as HTMLElement;
-
-        if (range.startOffset != 0) {
-            splitAt(firstElem, range.startOffset);
-            startPos++;
-            endPos++;
-        }
-        if (range.endOffset != 0) {
-            splitAt(lastElem, range.endOffset);
-            endPos++;
-        }
+        if (!isCuttingStart(range.startOffset, firstElem)) splitAt(firstElem, parent, range.startOffset);
+        if (!isCuttingEnd(range.endOffset, lastElem)) splitAt(lastElem, parent, range.endOffset);
     }
 
-    const rng = document.createRange();
-    rng.setStart(parent, startPos);
-    rng.setEnd(parent, endPos);
-    sel.removeAllRanges();
-    sel.addRange(rng);
-
-    for (let i = rng.startOffset; i < rng.endOffset; i++) {
-        if (parent.childNodes[i].nodeType == Node.ELEMENT_NODE) {
-            const child = parent.childNodes[i] as Element;
-            switch (format.type) {
-                case FormattingType.FOR_COLOR:
-                    changeClass(child, FormattingType.FOR_COLOR, format.value as string);
-                    break;
-                case FormattingType.BACK_COLOR:
-                    changeClass(child, FormattingType.BACK_COLOR, format.value as string);
-                    break;
-                case FormattingType.THICKNESS:
-                    changeClass(child, FormattingType.THICKNESS, format.value as string);
-                    break;
-                case FormattingType.BLINKING:
-                    child.classList.toggle(FormattingType.BLINKING, format.value as boolean);
-                    break;
-                case FormattingType.CROSSED:
-                    child.classList.toggle(FormattingType.CROSSED, format.value as boolean);
-                    break;
-                case FormattingType.UNDERCROSSED:
-                    child.classList.toggle(FormattingType.UNDERCROSSED, format.value as boolean);
-                    break;
-                case FormattingType.ITALIC:
-                    child.classList.toggle(FormattingType.ITALIC, format.value as boolean);
-                    break;
-            }
+    for (const child of parent.children) if (sel.containsNode(child) ||
+            isThisStart(child, firstElem, range.startOffset) ||
+            isThisEnd(child, lastElem, range.endOffset)) {
+        switch (format.type) {
+            case FormattingType.FOR_COLOR:
+                changeClass(child, FormattingType.FOR_COLOR, format.value as string);
+                break;
+            case FormattingType.BACK_COLOR:
+                changeClass(child, FormattingType.BACK_COLOR, format.value as string);
+                break;
+            case FormattingType.THICKNESS:
+                changeClass(child, FormattingType.THICKNESS, format.value as string);
+                break;
+            case FormattingType.BLINKING:
+                child.classList.toggle(FormattingType.BLINKING, format.value as boolean);
+                break;
+            case FormattingType.CROSSED:
+                child.classList.toggle(FormattingType.CROSSED, format.value as boolean);
+                break;
+            case FormattingType.UNDERCROSSED:
+                child.classList.toggle(FormattingType.UNDERCROSSED, format.value as boolean);
+                break;
+            case FormattingType.ITALIC:
+                child.classList.toggle(FormattingType.ITALIC, format.value as boolean);
+                break;
         }
     }
 }
